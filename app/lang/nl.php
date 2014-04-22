9fed46beb22c339afd13e124ad1be5996cee2992
Oguzhan Uysal <development@oguzhanuysal.eu>
2014-04-22 23:22:58 +0200

    added dutch locale

---
+++ app/lang/nl.php
@@ -0,0 +1,17 @@
+<?php
+
+$messages = array();
+$messages['title'] = 'Verkort!';
+$messages['have_fun_using'] = 'Veel plezier.';
+$messages['shorten_btn'] = 'Verkort';
+$messages['header_description'] = 'URL Verkort dienst';
+$messages['header_author'] = 'Murat Küçükosman, co-autheur Oguzhan Uysal';
+$messages['header_title'] = 'Dolap.co URL Verkort dienst';
+$messages['nav_index'] = 'Hoofdpagina';
+$messages['nav_new'] = 'Nieuwe URL';
+$messages['nav_about'] = 'Over ons';
+$messages['nav_contact'] = 'Contact';
+$messages['nav_title'] = 'u.Dolap.co';
+$messages['redirecting'] = 'Omleiden';
+$messages['checkbox_password_protect'] = 'Met wachtwoord beveiligen';
+$messages['password'] = "Wachtwoord";
