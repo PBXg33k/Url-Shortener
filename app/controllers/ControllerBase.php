61ac5a798b9243879e29ad6128d607b203715219
Oguzhan Uysal <development@oguzhanuysal.eu>
2015-04-11 00:26:53 +0200

    Added support for CloudFlare

--- app/controllers/ControllerBase.php
+++ app/controllers/ControllerBase.php
@@ -3,7 +3,9 @@
 class ControllerBase extends \Phalcon\Mvc\Controller {
 
     public function getUserIP() {
-        if (array_key_exists('HTTP_X_FORWARDED_FOR', $_SERVER) && !empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        if(isset($_SERVER['HTTP_CF_CONNECTING_IP'])) {
+            return $_SERVER['HTTP_CF_CONNECTING_IP'];
+        } elseif (array_key_exists('HTTP_X_FORWARDED_FOR', $_SERVER) && !empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
             if (strpos($_SERVER['HTTP_X_FORWARDED_FOR'], ',') > 0) {
                 $addr = explode(",", $_SERVER['HTTP_X_FORWARDED_FOR']);
                 return trim($addr[0]);
